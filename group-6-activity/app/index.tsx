1abf62e6fd72bbf157de7dd1742bb6cde2254a22
Jerjet1 <133961975+Jerjet1@users.noreply.github.com>
2025-05-26 21:39:15 +0800

    location string and request permission

--- group-6-activity/app/index.tsx
+++ group-6-activity/app/index.tsx
@@ -1,17 +1,28 @@
-import { Text, View, Pressable, ToastAndroid, TouchableOpacity } from "react-native";
-import React, {useState, useRef, useCallback} from "react"
-import {Camera, CameraType, CameraView, useCameraPermissions} from 'expo-camera';
-import * as Location from 'expo-location';
-import Icon from '@expo/vector-icons/FontAwesome5'
+import {
+  Text,
+  View,
+  Pressable,
+  ToastAndroid,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useRef, useCallback } from "react";
+import {
+  Camera,
+  CameraType,
+  CameraView,
+  useCameraPermissions,
+} from "expo-camera";
+import * as Location from "expo-location";
+import Icon from "@expo/vector-icons/FontAwesome5";
 import PhotoPreviewSection from "@/components/PhotoPreviewSection";
-import '../global.css'
+import "../global.css";
 
 export default function Index() {
-  const [facing, setFacing] = useState<CameraType>('back')
-  const [permission, setPermission] = useCameraPermissions()
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, setPermission] = useCameraPermissions();
   const [photo, setPhoto] = useState<any>(null);
   const cameraRef = useRef<CameraView | null>(null);
-  const [location, setLocation] = useState<{latitude: number, longitude: number} | null>(null)
+  const [location, setLocation] = useState("");
 
   const handleRequestPermission = async () => {
     const result = await setPermission();
@@ -20,14 +31,16 @@ export default function Index() {
     }
   };
 
-  if(!permission){
-    return <View/>
+  if (!permission) {
+    return <View />;
   }
 
-  if(!permission.granted){
+  if (!permission.granted) {
     return (
       <View className="flex-1 justify-center items-center">
-        <Text className="text-center">We need your permission to show the camera</Text>
+        <Text className="text-center">
+          We need your permission to show the camera
+        </Text>
         <Pressable
           onPress={handleRequestPermission}
           className="w-[120px] h-[50px] bg-blue-400 justify-center items-center">
@@ -37,24 +50,40 @@ export default function Index() {
     );
   }
 
-  const toggleCamera = () => {setFacing(current => (current === 'back' ? 'front' : 'back'));}
+  const toggleCamera = () => {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  };
 
   const handleTakePhoto = async () => {
     try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        ToastAndroid.show("Location permission denied", ToastAndroid.SHORT);
+        return;
+      }
       const [photo, currentLocation] = await Promise.all([
-        cameraRef.current?.takePictureAsync({ quality: 0.5, base64: false, exif: false }),
-        Location.getCurrentPositionAsync({})
+        cameraRef.current?.takePictureAsync({
+          quality: 0.5,
+          base64: false,
+          exif: false,
+        }),
+        Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.Highest,
+        }),
       ]);
-
-      setLocation({
-        latitude: currentLocation.coords.latitude,
-        longitude: currentLocation.coords.longitude
-      });
+      const [address] = await Location.reverseGeocodeAsync(
+        currentLocation.coords
+      );
+      const localString = `${address.formattedAddress}`;
+      setLocation(localString);
 
       setPhoto(photo);
     } catch (err) {
       console.log("Error taking photo or getting location", err);
-      ToastAndroid.show("Failed to take photo or get location", ToastAndroid.SHORT);
+      ToastAndroid.show(
+        "Failed to take photo or get location",
+        ToastAndroid.SHORT
+      );
     }
   };
 
@@ -66,7 +95,7 @@ export default function Index() {
   //         ToastAndroid.show("Location permission denied", ToastAndroid.SHORT);
   //         return
   //       }
-        
+
   //       const currentLocation = await Location.getCurrentPositionAsync({});
   //       setLocation({
   //         latitude: currentLocation.coords.latitude,
@@ -88,21 +117,34 @@ export default function Index() {
   //   }
   // };
 
-  const handleRetakePhoto =() => {setPhoto(null), setLocation(null)};
+  const handleRetakePhoto = () => {
+    setPhoto(null), setLocation("");
+  };
 
-  if (photo && location) return <PhotoPreviewSection photo={photo} location={location} handleRetakePhoto={handleRetakePhoto} />
+  if (photo && location)
+    return (
+      <PhotoPreviewSection
+        photo={photo}
+        location={location}
+        handleRetakePhoto={handleRetakePhoto}
+      />
+    );
 
   return (
     <View className="flex-1 justify-center bg-gray-200">
       <CameraView facing={facing} ref={cameraRef} className="flex-1">
-        <View className="flex-1 flex-row bg-transparent m-80"/>
+        <View className="flex-1 flex-row bg-transparent m-80" />
       </CameraView>
       <View className="flex flex-row justify-center items-center mt-10 gap-10">
-        <TouchableOpacity className="bg-white rounded-full border w-[60px] h-[60px] items-center justify-center" onPress={toggleCamera}>
-          <Icon name='retweet' size={24} color='black'/>
+        <TouchableOpacity
+          className="bg-white rounded-full border w-[60px] h-[60px] items-center justify-center"
+          onPress={toggleCamera}>
+          <Icon name="retweet" size={24} color="black" />
         </TouchableOpacity>
-        <TouchableOpacity className="bg-white rounded-full border w-[60px] h-[60px] items-center justify-center" onPress={handleTakePhoto}>
-          <Icon name='camera' size={24} color='black'/> 
+        <TouchableOpacity
+          className="bg-white rounded-full border w-[60px] h-[60px] items-center justify-center"
+          onPress={handleTakePhoto}>
+          <Icon name="camera" size={24} color="black" />
         </TouchableOpacity>
       </View>
     </View>
--- group-6-activity/components/PhotoPreviewSection.tsx
+++ group-6-activity/components/PhotoPreviewSection.tsx
@@ -1,76 +1,80 @@
-import { Fontisto } from '@expo/vector-icons';
-import { CameraCapturedPicture } from 'expo-camera';
-import React from 'react'
-import { TouchableOpacity, SafeAreaView, Image, StyleSheet, View, Text } from 'react-native';
+import { Fontisto } from "@expo/vector-icons";
+import { CameraCapturedPicture } from "expo-camera";
+import * as Location from "expo-location";
+import React from "react";
+import {
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  View,
+  Text,
+} from "react-native";
 
 const PhotoPreviewSection = ({
-    photo,
-    location,
-    handleRetakePhoto
+  photo,
+  location,
+  handleRetakePhoto,
 }: {
-    photo: CameraCapturedPicture;
-    location: {latitude: number; longitude: number};
-    handleRetakePhoto: () => void;
+  photo: CameraCapturedPicture;
+  location: String;
+  handleRetakePhoto: () => void;
 }) => (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.box}>
-            <Image
-                style={styles.previewConatiner}
-                source={{uri: photo.uri}}
-            >
-            </Image>
-        </View>
-        
-        <Text style={styles.locationText}>
-            Latitude: {location.latitude.toFixed(5)} | Longitude: {location.longitude.toFixed(5)}
-        </Text>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={handleRetakePhoto}>
-                <Fontisto name='trash' size={36} color='black' />
-            </TouchableOpacity>
-        </View>
-    </SafeAreaView>
+  <SafeAreaView style={styles.container}>
+    <View style={styles.box}>
+      <Image
+        style={styles.previewConatiner}
+        source={{ uri: photo.uri }}></Image>
+      <Text style={styles.locationText}>{location}</Text>
+    </View>
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity style={styles.button} onPress={handleRetakePhoto}>
+        <Fontisto name="trash" size={36} color="black" />
+      </TouchableOpacity>
+    </View>
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    box: {
-        borderRadius: 15,
-        padding: 1,
-        width: '95%',
-        backgroundColor: 'darkgray',
-        justifyContent: 'center',
-        alignItems: "center",
-    },
-    previewConatiner: {
-        width: '95%',
-        height: '85%',
-        borderRadius: 15
-    },
-    buttonContainer: {
-        marginTop: '4%',
-        flexDirection: 'row',
-        justifyContent: "center",
-        width: '100%',
-    },
-    button: {
-        backgroundColor: 'gray',
-        borderRadius: 25,
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    locationText: {
-    color: 'white',
-    fontSize: 16,
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    borderRadius: 15,
+    padding: 1,
+    width: "95%",
+    backgroundColor: "darkgray",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  previewConatiner: {
+    width: "95%",
+    height: "75%",
+    borderRadius: 15,
+  },
+  buttonContainer: {
+    marginTop: "4%",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "gray",
+    borderRadius: 25,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 45,
+  },
+  locationText: {
+    color: "white",
+    fontSize: 14,
     marginVertical: 10,
-    textAlign: 'center',
-}
+    textAlign: "center",
+  },
 });
 
 export default PhotoPreviewSection;
